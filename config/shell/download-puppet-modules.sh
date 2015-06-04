e16083afa6006b4c07163a8006441a2c4f837849
SmthDiff <admin@smthdiff.com>
2015-06-04 23:30:22 +0200

    [BUGFIX] added shell comments

--- config/shell/download-puppet-modules.sh
+++ config/shell/download-puppet-modules.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+echo "Creating Puppet Modules Folder"
 mkdir -p /etc/puppet/modules;
+echo "Finished creating Puppet Modules Folder"
 
 if [ ! -d /etc/puppet/modules/apache ]; then
+	echo "Adding Apache Module"
 	puppet module install puppetlabs-apache
+	echo "Finished adding Apache Module"
 fi
