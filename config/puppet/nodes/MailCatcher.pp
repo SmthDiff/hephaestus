b36b7acf3fcc22ba3b86a6f637e01e6c1c7abf7f
SmthDiff <admin@smthdiff.com>
2015-07-07 22:56:24 +0200

    [BUGFIX] start mailcatcher with system

--- config/puppet/nodes/MailCatcher.pp
+++ config/puppet/nodes/MailCatcher.pp
@@ -1,3 +1,5 @@
-class {'mailcatcher': 
-	service_enable => true,
+class mailcatcher::params {
+  	$smtp_ip          = '192.168.33.101'
+  	$http_ip          = '192.168.33.101'
+	$service_enable = true
 }
